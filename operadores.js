23da90e1bb5f7a269e3978a443f8ee830a97c1a5
Cesar <cesar.a.fior@hotmail.com>
2024-01-24 22:38:19 -0300

    novo arqiovo

---
+++ operadores.js
@@ -0,0 +1,5 @@
+let idade = 21
+let paisPresentes = false;
+let comprouBilhete = false;
+const podeViajar = idade > 18 || (paisPresentes && comprouBilhete);
+console.log('Pode viajar: ${podeViajar}');
